ac9313ac7132b80e7f094b7b177bccdde908ade4
krawczyk-marta <marta.krawczyk@kcl.ac.uk>
2018-11-26 12:34:36 +0000

    change folder structure (private & public)

--- public/product.php
+++ public/product.php
@@ -1,14 +1,14 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <!-- Required meta tags -->
+    &lt;!&ndash; Required meta tags &ndash;&gt;
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" type="text/css" href="css/style.css">
-    <!-- Bootstrap CSS -->
+    &lt;!&ndash; Bootstrap CSS &ndash;&gt;
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="css/selection.css">
-    <title>Computer Gaming Society</title>
+    <title>PRODUCT NAME</title>
 </head>
 <body class="background-img">
 <div class="container">
@@ -25,7 +25,7 @@
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                         <a class="dropdown-item" href="home_page.html">Home</a>
-                        <a class="dropdown-item" href="collection.html">Collection</a>
+                        <a class="dropdown-item" href="collection.html.html">Collection</a>
                         <a class="dropdown-item" href="#">Dummy</a>
                         <a class="dropdown-item" href="login.html.html">Log In</a>
                     </div>
@@ -37,25 +37,49 @@
     <div class="row white">
         <div class="col-md-3"></div>
         <div class="col-md-6">
-            <h1>Collection</h1>
+            <h1>PRODUCT NAME</h1>
         </div>
-        <div class="col-md-3">
-            <div class="center">
-    <select name="sources" id="sources" class="custom-select sources" placeholder="Source Type">
-    <option value="profile">Avilable</option>
-    <option value="word">ForKids</option>
-    <option value="hashtag">Movie</option>
-  </select>
-</div></div>
+        <div class="col-md-3"></div>
+    </div>
+
+    <div class="row py-5">
+        <div class="col-md-6">
+            <img src="https://pixel.nymag.com/imgs/fashion/daily/2018/03/02/magazine/how-to-raise-a-boy/05-video-game-dad-lede.w700.h700.jpg" alt="Minecraft" width="500" height="600">
+        </div>
+        <div class="col-md-6">
+            <div class="row">
+                <div class="col-md-12">
+                    <p class="white">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <p class="white-text">RATING FROM API</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <p class="white-text">STATUS: available (3)/out of stock</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <p class="white-text">AGE: 7+</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <p class="white-text">PLATFORM: Play Station 4</p>
+                </div>
+            </div>
+
+
+
+        </div>
+
     </div>
 
 
-
-    <div class="row">
-
-        <include src="main_body.html"></include>
-  
-    </div>
 
 
 
@@ -90,7 +114,7 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-     <script src="js/include.js"></script>
+<script src="../js/include.js"></script>
 </body>
 </html>
 
