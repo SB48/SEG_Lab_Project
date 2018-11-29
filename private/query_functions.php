6e1237ec989ca8a3e9475e0ca738730576a53615
Jay Macpherson <jaymacpherson93@gmail.com>
2018-11-29 01:51:34 +0000

    find all members query

--- private/query_functions.php
+++ private/query_functions.php
@@ -8,9 +8,19 @@
 
     function find_all_games(){
         global $db;
-        $sql = "SELECT * FROM Game";
+        $sql = "SELECT * FROM Game ";
         $sql .= "ORDER BY name ASC";
         $result = mysqli_query($db, $sql);
+        confirm_result_set($result);
+        return $result;
+    }
+
+    function find_all_members(){
+        global $db;
+        $sql = "SELECT * FROM Member ";
+        $sql = "ORDER BY name ASC";
+        $result = mysqli_query($db,$sql);
+        confirm_result_set($result);
         return $result;
     }
 
