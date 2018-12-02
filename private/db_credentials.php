74657b15625ad20010a0f68ac30d123a70d3fd2d
Jay Macpherson <jaymacpherson93@gmail.com>
2018-12-02 22:52:32 +0000

    pathing error fix, works on XAMPP

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -6,7 +6,7 @@
  * Time: 11:18
  */
 define("DB_SERVER", "localhost");//change into IP of remote server
-define("DB_USER", "webuser"); //change into our username
+define("DB_USER", "root"); //change into our username
 define("DB_PASS", ""); //change into our password
 define("DB_NAME", "gamesoc"); //change into out db name
 
--- public/staff/volunteer/volunteer_platform.php
+++ public/staff/volunteer/volunteer_platform.php
@@ -1,5 +1,5 @@
 
-<?php require_once('../private/initialize.php'); ?>
+<?php require_once('../../../private/initialize.php'); ?>
 <?php require_once(SHARED_PATH . '/header.php'); ?>
 
 <div class="container">
