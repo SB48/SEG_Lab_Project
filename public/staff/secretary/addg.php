8a7a47863684d47942d5e8448c46e0edb5220246
tao lin <38518077+alexlint@users.noreply.github.com>
2018-12-04 16:11:32 +0000

    Update addg.php

--- public/staff/secretary/addg.php
+++ public/staff/secretary/addg.php
@@ -1,23 +1,8 @@
 
 <?php
-$mysqli = new mysqli('localhost', 'my_user', 'my_password', 'my_db');
-if (!$mysqli)
-{
-    die('Could not connect: ' .$mysqli->connect_error());
-}
-
-$mysqli->select_db("my_db");
-
+global $db;
 $sql="INSERT INTO Game (name, ageRating,genre,url,platform)
 VALUES
 ('$_POST[name]','$_POST[price]','$_POST[copies]','$_POST[URL]','$_POST[age]','$_POST[platform]')";
 
-if (!$mysqli->query($sql))
-{
-    die('Error: ' .$mysqli->error());
-}
-echo "1 record added";
-
-$mysqli->close()
 ?>
-
