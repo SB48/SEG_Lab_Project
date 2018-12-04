72b527a39ce35732d5bebcb08f85c82a71561bac
tao lin <38518077+alexlint@users.noreply.github.com>
2018-12-04 16:11:10 +0000

    Update addm.php

--- public/staff/addm.php
+++ public/staff/addm.php
@@ -1,22 +1,7 @@
 
 <?php
-$mysqli = new mysqli('localhost', 'my_user', 'my_password', 'my_db');
-if (!$mysqli)
-{
-    die('Could not connect: ' .$mysqli->connect_error());
-}
-
-$mysqli->select_db("my_db");
-
+global $db;
 $sql="INSERT INTO Member(firstName, lastName,dob)
 VALUES
 ('$_POST[FirstName]','$_POST[SurName]','$_POST[bday]')";
-
-if (!$mysqli->query($sql))
-{
-    die('Error: ' .$mysqli->error());
-}
-echo "1 record added";
-
-$mysqli->close()
 ?>
