7304acaf8945ec384693d4e9dfec026b7ad0f989
Marta Krawczyk <marta.krawczyk@kcl.ac.uk>
2018-12-06 15:19:34 +0000

    Marta's fuctions added

--- private/query_functions.php
+++ private/query_functions.php
@@ -132,5 +132,61 @@
         confirm_result_set($thisGame_set);
         return $thisGame_set;
     }
-
+    function find_member_name($memberID){
+        global $db;
+        $sql = "SELECT firstName, lastName FROM Member WHERE memberID = $memberID";
+        $result = mysqli_query($db,$sql);
+        confirm_result_set($result);
+        return $result;
+    }
+    function find_how_many_games_are_rented($memberID){
+        global $db;
+        $sql = "SELECT COUNT(rentalID) FROM Member WHERE memberID = $memberID AND returned = false";
+        $result = mysqli_query($db,$sql);
+        confirm_result_set($result);
+        return $result;
+    }
+    function find_how_many_games_can_rent(){
+        global $db;
+        $sql = "SELECT value FROM Rules WHERE rule = 'rentalLimit'";
+        $result = mysqli_query($db,$sql);
+        confirm_result_set($result);
+        return $result;
+    }
+    function find_is_banned($memberID){
+        global $db;
+        $sql = "SELECT normalBan FROM Member WHERE memberID = $memberID";
+        $result = mysqli_query($db,$sql);
+        confirm_result_set($result);
+        return $result;
+    }
+    function find_violations_possible(){
+        global $db;
+        $sql = "SELECT value FROM Rules WHERE rule = 'numViolationsForBan'";
+        $result = mysqli_query($db,$sql);
+        confirm_result_set($result);
+        return $result;
+    }
+    function find_violations_in_grace_period($memberID){
+        global $db;
+        $sql = "SELECT COUNT(memberID) FROM Violates WHERE memberID = $memberID AND nullified = false";
+        $result = mysqli_query($db,$sql);
+        confirm_result_set($result);
+        return $result;
+    }
+    function find_amount_due($memberID){
+        global $db;
+        $sql = "SELECT amountDue FROM Member WHERE memberID = $memberID";
+        $result = mysqli_query($db,$sql);
+        confirm_result_set($result);
+        return $result;
+    }
+    function find_current_rentals($memberID){
+        global $db;
+        $sql ="SELECT gameID, name, returnDate FROM Rental, Game WHERE memberID = $memberID AND returned = false AND Rental.gameID = Game.gameID";
+        $result = mysqli_query($db,$sql);
+        confirm_result_set($result);
+        return $result;
+    }
+    
 ?>
