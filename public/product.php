777cba7a7a8e19f5e571282cf80cde22e80e3d0c
Sophie Biber <biberwood@gmail.com>
2018-12-07 16:11:32 +0000

    Game Information Displayed on Click
    Fixed error - so that when clicking on game on collection page, it send ID through to display information

--- public/product.php
+++ public/product.php
@@ -1,6 +1,6 @@
 
-<?php require_once(PRIVATE_PATH . '/initialize.php'); ?>
-<?php require_once(SHARED_PATH . '/header.php'); ?>
+<?php require_once('../private/initialize.php'); ?>
+<?php require_once('../private/shared/header.php'); ?>
 
 <div class="container">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -30,20 +30,20 @@
     //id as int (instead of string)
     $id = (int) $_GET['id'];
     $id = isset($_GET['id']) ? (int) $_GET['id'] : 1;
-
     $thisGameSet = find_game($id);
     $thisGame = mysqli_fetch_assoc($thisGameSet);
 
-    $gameID = $thisGame['id']
-    $gameName = $thisGame['name']
-    $gamePrice = $thisGame['price']
-    $gameAgeRating = $thisGame['ageRating']
-    $gameGenre = $thisGame['genre']
-    $gameDescription = $thisGame['description']
-    $gameCopies = $thisGame['copies']
-    $gameURL = $thisGame['url']
-    $gamePicPath = $thisGame['path']
-    $gamePlatform = $thisGame['platform']
+
+    $gameID = $thisGame['gameID'];
+    $gameName = $thisGame['name'];
+    $gamePrice = $thisGame['price'];
+    $gameAgeRating = $thisGame['ageRating'];
+    $gameGenre = $thisGame['genre'];
+    $gameDescription = $thisGame['description'];
+    $gameCopies = $thisGame['copies'];
+    $gameURL = $thisGame['url'];
+    $gamePicPath = $thisGame['path'];
+    $gamePlatform = $thisGame['platform'];
 
     ?>
 
@@ -58,7 +58,7 @@
 
     <div class="row py-5">
         <div class="col-md-6">
-            <?php echo '<img src="'.$eachGame["path"]. '" width="100px" class="productImage">' ?>
+            <?php echo '<img src="'.$thisGame["path"]. '" width="100px" class="productImage">' ?>
         </div>
         <div class="col-md-6">
             <div class="row">
@@ -97,4 +97,4 @@
 
 
 
-    <?php require_once(SHARED_PATH . '/footer.php'); ?>
+<?php require_once('../private/shared/footer.php'); ?>
